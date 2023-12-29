9d6eb297f8c38b7a1f4e6be3b1870b3135194509
kubzzz <martijnstrolenberg@live.nl>
2023-12-29 11:44:25 +0100

    Added some arduino libraries

--- balance_robot_arduino/src/main.cpp
+++ balance_robot_arduino/src/main.cpp
@@ -1,24 +1,40 @@
 /*
- * I2C Device at address 0x1E, 0x68, 0x7E found
- *  
+ * Balance robot main arduino code
  * 
+ * Authors: 
+ * Martijn Strolenberg
+ * Tom Janssen-Bouwmeester
+ * 
+ * I2C Device at address 0x1E, 0x68, 0x7E found
  *
  */
 #include <Arduino.h>
-#include <Wire.h>
-#include <ICM42688.h>
+#include <ICM42688.h>     // ICM42688 IMU Library
+//#include <TMC2209.h>      // TMC2209 stepper drivers Library
+#include <AccelStepper.h> // Accel Stepper Library 
 
-#define LED_PIN 13
-#define WIRE Wire1 // wWre=SDA,SCL Wire1=SDA1,SCL1
+#define LED_PIN 13 // Built in LED of Arduino
+#define WIRE Wire1 // Wire=SDA,SCL Wire1=SDA1,SCL1
 
+void print_IMU_data(void);
+
+/* Making IMU object with I2C channel (Wire1) and address (0x68) */
 ICM42688 IMU(WIRE, 0x68);
+
+/* Making an motor object where pin(11)=STEP pin(12)=DIR */
+AccelStepper motorR(1, 11, 12);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200); // begin serial communication to read data from the arduino
   while (!Serial) {}
+
+  motorR.setEnablePin(8);
+  motorR.setMaxSpeed(500);
+  motorR.setSpeed(50);
   
   int status = IMU.begin();
+  /* Check if the IMU is correctly working */
   if (status < 0) {
     Serial.println("IMU initialization unsuccessful");
     Serial.println("Check IMU wiring or try cycling power");
@@ -44,9 +60,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  motorR.runSpeed();
+
+  
+}
+
+
+void print_IMU_data(void)
+{
   // read the sensor
   IMU.getAGT();
-
   // display the data
   Serial.print(IMU.accX(),6);
   Serial.print("\t");
@@ -63,4 +86,3 @@ void loop() {
   Serial.println(IMU.temp(),6);
   delay(20);
 }
-
